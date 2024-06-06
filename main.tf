29fdd6b3989f4294f9440b2649b3e85439fe0406
harish kandula <Hkandul@tbdir.net>
2024-06-07 00:55:11 +0530

    init commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0"
+    }
+  }
+
+  # Update this block with the location of your terraform state file
+  backend "azurerm" {
+    resource_group_name  = "test-rg"
+    storage_account_name = "testhrsa"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
+
+# Define any Azure resources to be created here. A simple resource group is shown here as a minimal example.
+resource "azurerm_resource_group" "rg-aks" {
+  name     = var.resource_group_name
+  location = var.location
+}
