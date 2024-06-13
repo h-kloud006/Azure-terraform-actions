6279b61e62ac545c4e3e7c9e82688490283ebcae
harish kandula <Hkandul@tbdir.net>
2024-06-13 18:22:06 +0530

    adding azure pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,18 +5,8 @@ terraform {
       version = ">= 3.7.0"
     }
   }
-
-  # Update this block with the location of your terraform state file
-  backend "azurerm" {
-    resource_group_name  = "test-rg"
-    storage_account_name = "testhrsa"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-    use_azuread_auth = true 
-  }
 }
-
-
+  # Update this block with the location of your terraform state file
 # Define any Azure resources to be created here. A simple resource group is shown here as a minimal example.
 resource "azurerm_resource_group" "rg-aks" {
   name     = var.resource_group_name
