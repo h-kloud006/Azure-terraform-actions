390edee1742a10596dfb494956198882eb948af8
harish kandula <Hkandul@tbdir.net>
2024-06-14 17:57:41 +0530

    fix

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,3 +1,3 @@
 locals {
-  openai_endpoint_fqdn = "${azurerm_private_endpoint.openai_private_endpoint.name}.${eastus}.azurewebsites.net"
+  openai_endpoint_fqdn = "${azurerm_private_endpoint.openai_private_endpoint.name}.eastus.azurewebsites.net"
 }
