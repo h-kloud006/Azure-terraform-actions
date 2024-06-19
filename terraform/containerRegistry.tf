9ea562af359f0d2ac440f0a3629082c60c5ff2e3
harish kandula <Hkandul@tbdir.net>
2024-06-19 13:50:47 +0530

    fix

--- terraform/containerRegistry.tf
+++ terraform/containerRegistry.tf
@@ -1,6 +1,6 @@
 
 resource "azurerm_container_registry" "backend_registry" {
-  name                = "backend"
+  name                = "hrbackend"
   resource_group_name = "test-rg"
   location            = "eastus"
   sku                 = "Standard"  
